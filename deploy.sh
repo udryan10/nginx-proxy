5aec52357c389332453ca259f1b1bc4b9d7c1de0
udryan10 <rgrothouse@gmail.com>
2014-12-13 13:30:02 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker kill nginx-proxy
 docker rm nginx-proxy
 docker build -t nginx-proxy .
-docker run -d --name="nginx-proxy" -p 80:80 nginx-proxy
+docker run -d --restart="always" --name="nginx-proxy" -p 80:80 nginx-proxy
