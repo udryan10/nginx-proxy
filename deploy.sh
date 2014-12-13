a1ea49f525d5cf40a34f8882e061ee874fc37568
udryan10 <rgrothouse@gmail.com>
2014-12-13 13:23:19 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 docker kill nginx-proxy
 docker rm nginx-proxy
-docker build -t lngarrett/nginx-proxy .
-docker run -d --name="nginx-proxy" -p 80:80 lngarrett/nginx-proxy
+docker build -t nginx-proxy .
+docker run -d --name="nginx-proxy" -p 80:80 nginx-proxy
